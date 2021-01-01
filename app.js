46c91ff875922d57faf16f3d77f8fc98c3ca278f
kokoyroy <kokoyroy@gmail.com>
2021-01-01 02:50:43 +0200

    webchat

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
+const server = app.listen(4000, () => {
+    console.log('listening to requests on port 4000');
+})
+
+
+//static files
+app.use(express.static(__dirname + '/public'))
+
+app.post('/', (req, res) => {
+    res.render('/index.html')
+})
+//socket setup
+
+var io = socket(server)
+io.on('connection', (socket) => {
+    console.log('connection established');
+    console.log(socket.id);
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+    socket.on('feed', (data) => {
+        socket.broadcast.emit('feed', data)
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,65 @@
+
+// make connection
+var socket = io.connect('http://192.168.100.2:4000/')
+var timer
+
+
+//query Dom
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+//utility funcs
+function emit() {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    message.value = ''
+}
+function broadcast() {
+    clearTimeout(timer)
+    socket.emit('feed', { handle: handle.value })
+}
+
+
+
+
+//emit event
+
+btn.addEventListener('click', () => {
+    console.log('patithike to koumpi');
+    emit()
+})
+message.addEventListener('keypress', (params) => {
+    if (params.key === 'Enter') {
+        emit()
+    } else {
+
+        broadcast()
+    }
+})
+
+//receive events
+socket.on('chat', data => {
+    feedback.innerHTML='';
+    console.log(data);
+    if (data.handle === handle.value) {
+        return output.innerHTML += '<p><strong>' + data.handle +
+            ':   ' + '</strong>'
+            + data.message + '</p>'
+    }
+    else {
+        output.innerHTML += '<p class="ksenos"><strong>' + data.handle +
+            ':   ' + '</strong>'
+            + data.message + '</p>'
+    }
+})
+
+socket.on('feed', data => {
+    feedback.innerHTML += `<p><em> ${data} is typing...</em></p>`
+})
+
+
